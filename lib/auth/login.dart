560774485bb2eaf11b581ed6e95f6d2dea300fab
ThanhB1909985 <thanhprolatui@gmail.com>
2023-03-12 21:13:42 +0700

    Implement register function and show_snack_bar, share_preference is just an idel for now and not yet implemented

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -3,6 +3,7 @@ import 'package:myproject_app/auth/conponents/input.dart';
 import 'package:myproject_app/auth/conponents/auth_button.dart';
 import 'package:myproject_app/home/home.dart';
 import 'package:myproject_app/services/auth.dart';
+import '../services/share_preference.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -13,6 +14,16 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  login() {
+    if (formKey.currentState!.validate()) {}
+  }
+
+  register() {
+    Navigator.pushReplacementNamed(context, '/register');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +31,9 @@ class _LoginState extends State<Login> {
       stream: Auth().userStream,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Text("Loading");
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         } else if (snapshot.hasError) {
           return const Text("error");
         } else if (snapshot.hasData) {
@@ -49,24 +62,26 @@ class _LoginState extends State<Login> {
                   const SizedBox(
                     height: 10,
                   ),
-                  const Input(
+                  Input(
                     label: "Email",
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.email,
                     ),
                     showInput: true,
                     validator: "email",
+                    controller: emailController,
                   ),
                   const SizedBox(
                     height: 15,
                   ),
-                  const Input(
+                  Input(
                     label: "Password",
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.lock,
                     ),
                     showInput: false,
                     validator: "password",
+                    controller: passwordController,
                   ),
                   const SizedBox(
                     height: 15,
@@ -99,13 +114,5 @@ class _LoginState extends State<Login> {
         );
       },
     );
-  }
-
-  login() {
-    print(formKey.currentState!.validate());
-  }
-
-  register() {
-    Navigator.pushReplacementNamed(context, '/register');
   }
 }
